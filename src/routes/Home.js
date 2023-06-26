9f9b721f548af305ed4c386d3463f75d33878feb
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 14:06:31 +0300

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,44 @@
-const Home = () => <h1 className="text-3xl font-bold underline">home</h1>;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchIS from '../redux/thunk';
+import { selectApple } from '../redux/store';
+import Companies from '../components/companies';
+import GET_APPLE from '../redux/api';
+
+const Home = () => {
+  const {
+    appleIS, isLoading, error, errMsg,
+  } = useSelector(selectApple);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchIS({ url: GET_APPLE }));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>
+          Oops! an error occurred. Please try again.
+          {errMsg}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Companies companies={appleIS} />
+    </div>
+  );
+};
+
 export default Home;
