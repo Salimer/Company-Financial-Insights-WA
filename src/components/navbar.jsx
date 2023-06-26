c8398c1864a5e2770760887f6f1a863f48589c49
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 17:36:15 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,38 @@
-import { Container } from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import styled from 'styled-components';
 
-const NavBar = () => (
-  <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const Navbar = () => {
+  const Links = [
+    { name: 'HOME', link: '#headline' },
+    { name: 'PROJECTS', link: '#projects' },
+    { name: 'ABOUT', link: '#about-me' },
+    { name: 'CONTACT', link: '#contact' },
+  ];
 
-export default NavBar;
+  return (
+    <Section className="shadow-md w-full top-0 left-0 bg-white">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
+          <span className="text-3xl text-indigo-600 mr-1 pt-2" />
+        </div>
+        <ul className="md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in">
+          {
+          Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a href={link.link} className="text-gray-800 hover:text-gray-400 duration-500">{link.name}</a>
+            </li>
+          ))
+        }
+        </ul>
+      </div>
+    </Section>
+  );
+};
+
+export default Navbar;
+
+const Section = styled.header`
+z-index: 999;
+ion-icon {
+  color: #6936f5;
+}
+`;
