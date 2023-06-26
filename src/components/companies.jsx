862a6954501da3953bc152c25fcdc8c956ce792b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 16:44:24 +0300

    Update companies.jsx

--- src/components/companies.jsx
+++ src/components/companies.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
 
 const Companies = ({ companies }) => {
   console.log(companies);
   const brands = ['apple', 'facebook', 'smasung', 'xiaomi', 'twitter'];
   return (
-    <section className="grid grid-cols-2">
+    <Section className="grid grid-cols-2">
       {brands.map((brand) => (
-        <button key={brand} className=" bg-black p-5 text-white" type="button">{brand}</button>
+        <Button key={brand}>{brand}</Button>
       ))}
-    </section>
+    </Section>
   );
 };
 
@@ -17,3 +19,9 @@ export default Companies;
 Companies.propTypes = {
   companies: PropTypes.arrayOf(Object).isRequired,
 };
+
+const Section = styled.section`
+&:nth-child(odd) {
+  background-color: #ddd;
+}
+`;
