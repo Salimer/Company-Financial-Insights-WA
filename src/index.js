8a4b0a91b42c532572f8b3600e7dfa4dd3e6e3ce
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 14:06:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provdier } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provdier store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provdier>
+    </Provider>
   </React.StrictMode>,
 );
 
