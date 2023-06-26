5e571908f6c15fbc507ed2ecc0e9138309a0c67d
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 23:36:52 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Button = ({ children }) => (
-  <button className=" bg-buttonFirstColor p-5 text-white" type="button">{children}</button>
+const Button = ({ children, url }) => (
+  <NavLink className="bg-buttonFirstColor p-5 text-white" to={`/${url}`}>{children}</NavLink>
 );
 export default Button;
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
