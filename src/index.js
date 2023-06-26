b0fedf33633855fe7910fcaf6027d7c1f706a901
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 17:36:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,4 +20,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
