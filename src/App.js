ca826623030bac85ed7310480bdb88cb5161c969
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-26 14:39:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './components/layout';
 import Home from './routes/Home';
 import Details from './routes/Details';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details" element={<Details />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Route>
     </Routes>
   );
 }
