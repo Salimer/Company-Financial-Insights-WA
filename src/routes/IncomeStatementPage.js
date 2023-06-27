3a6c70e6aad6f3a51bb8c1a912222d7bbde8c28a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-27 16:26:54 +0300

    Update IncomeStatementPage.js

--- src/routes/IncomeStatementPage.js
+++ src/routes/IncomeStatementPage.js
@@ -1,7 +1,49 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import fetchIS from '../redux/thunk';
+import { selectApple } from '../redux/store';
 import IncomeStatement from '../components/incomeStatement';
+import getCompanyURL from '../functions/getCompanyURL';
 
-const IncomeStatementPage = () => (
-  <IncomeStatement />
-);
+const IncomeStatementPage = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  const companyName = pathname.split('/')[1];
+  console.log(companyName);
+  const companyUrl = getCompanyURL(companyName);
+  const {
+    appleIS, isLoading, error, errMsg,
+  } = useSelector(selectApple);
+  const dispatch = useDispatch();
+
+  console.log(appleIS);
+  useEffect(() => {
+    dispatch(fetchIS({ url: companyUrl }));
+  }, [dispatch, appleIS.length]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>
+          Oops! an error occurred. Please try again.
+          {errMsg}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <IncomeStatement company={appleIS} />
+  );
+};
 
 export default IncomeStatementPage;
