084fac896312bdb46349a1efebb13388d77433d4
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-27 13:58:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/layout';
-import Revenue from './routes/Revenue';
-import Home from './routes/Home';
-import IncomeStatement from './routes/incomeStatement';
+import RevenuePage from './routes/RevenuePage';
+import HomePage from './routes/HomePage';
+import IncomeStatementPage from './routes/IncomeStatementPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/:companyName" element={<Revenue />} />
-        <Route path="/:companyName/:year" element={<IncomeStatement />} />
+        <Route index element={<HomePage />} />
+        <Route path="/:companyName" element={<RevenuePage />} />
+        <Route path="/:companyName/:year" element={<IncomeStatementPage />} />
       </Route>
     </Routes>
   );
