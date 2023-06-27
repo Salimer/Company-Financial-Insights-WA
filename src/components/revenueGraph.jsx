9397a7bd71a561cf1312d797b4704f351bd19553
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-27 13:13:58 +0300

    Update revenueGraph.jsx

--- src/components/revenueGraph.jsx
+++ src/components/revenueGraph.jsx
@@ -1,17 +1,25 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Button from './Button';
+import getCompanyName from '../functions/getCompanyName';
 
-const RevenueGraph = () => {
-  const years = [2020, 2021, 2022, 2023];
+const RevenueGraph = ({ company }) => {
+  console.log(company);
+  const companyName = getCompanyName(company[0].symbol);
+  // const years = [2020, 2021, 2022, 2023];
   const nthChildren = [1, 4, 5, 8];
   return (
     <Section nthChildren={nthChildren} className="grid grid-cols-2 w-full">
       <h1>Company revenue graph and years list</h1>
-      {years.map((year) => (
-        <Button key={year} url={`companyName/${year}`}>{year}</Button>
+      {company.map((year) => (
+        <Button key={year.date} url={`${companyName}/${year.date.substring(0, 4)}`}>{year.date.substring(0, 4)}</Button>
       ))}
     </Section>
   );
+};
+
+RevenueGraph.propTypes = {
+  company: PropTypes.arrayOf(Object).isRequired,
 };
 
 export default RevenueGraph;
