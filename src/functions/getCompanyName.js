e0a84207716d3b362234d4abface749ff523113e
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-27 16:27:01 +0300

    Update getCompanyName.js

--- src/functions/getCompanyName.js
+++ src/functions/getCompanyName.js
@@ -1,5 +1,5 @@
 export default (company) => {
-  const symbols = [['AAPL', 'apple']];
+  const symbols = [['AAPL', 'apple'], ['GOOG', 'google']];
   const matchingSymbol = symbols.find(([symbol]) => symbol === company);
   const companyName = matchingSymbol ? matchingSymbol[1] : '';
   return companyName;
