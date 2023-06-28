2b3dbc7d906d9384f7f03d97fbbc10ce1d17c4b7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 10:58:36 +0300

    Update getCompanyName.js

--- src/functions/getCompanyName.js
+++ src/functions/getCompanyName.js
@@ -1,6 +1,10 @@
+import companiesArr from './companiesArr';
+
 export default (company) => {
-  const symbols = [['AAPL', 'apple'], ['GOOG', 'google']];
-  const matchingSymbol = symbols.find(([symbol]) => symbol === company);
-  const companyName = matchingSymbol ? matchingSymbol[1] : '';
+  if (!company) {
+    return null; // or handle the case when company is undefined/null
+  }
+  const matchingSymbol = companiesArr.find(([, symbol]) => symbol === company);
+  const companyName = matchingSymbol ? matchingSymbol[0] : '';
   return companyName;
 };
