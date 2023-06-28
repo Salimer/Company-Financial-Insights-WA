65212f82dda7b7b040f78f284121241bd6810413
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 21:07:44 +0300

    Create revenueGraph.test.js

---
+++ src/__tests__/revenueGraph.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import RevenueGraph from '../components/revenueGraph';
+
+jest.mock('../functions/getCompanyName', () => jest.fn().mockReturnValue('Company Name'));
+
+describe('RevenueGraph', () => {
+  test('renders correctly', () => {
+    const company = [
+      { symbol: 'ABC', date: '2021-01-01' },
+      { symbol: 'ABC', date: '2022-01-01' },
+      { symbol: 'ABC', date: '2023-01-01' },
+    ];
+    const { container } = render(
+    <MemoryRouter>
+        <RevenueGraph company={company} />
+    </MemoryRouter>);
+    expect(container).toMatchSnapshot();
+  });
+});
