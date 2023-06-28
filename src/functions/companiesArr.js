7ca590df36215eba3a29dccb35114d5524d06fcc
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 17:48:04 +0300

    Update companiesArr.js

--- src/functions/companiesArr.js
+++ src/functions/companiesArr.js
@@ -3,4 +3,10 @@
 export default [
   ['apple', 'AAPL'],
   ['google', 'GOOG'],
+  ['meta', 'META'],
+  ['microsoft', 'MSFT'],
+  ['nvidia', 'NVDA'],
+  ['amd', 'AMD'],
+  ['xiaomi', 'XIACF'],
+  ['sony', 'SONY'],
 ];
