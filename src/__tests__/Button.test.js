d70ddc43ed7e4df802ec07172406b6c70611dfdd
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 21:08:00 +0300

    Create Button.test.js

---
+++ src/__tests__/Button.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Button from './../components/Button';
+
+describe('Button component', () => {
+  it('renders the button correctly', () => {
+    const props = {
+      children: 'Test Button',
+      url: 'test-url',
+      iconName: 'test-icon',
+    };
+    const { container } = render(
+    <MemoryRouter>
+        <Button {...props} />
+    </MemoryRouter>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
