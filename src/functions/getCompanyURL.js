b4238b55a3f1ca60195da8801b11d1b29c202873
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 10:58:34 +0300

    Update getCompanyURL.js

--- src/functions/getCompanyURL.js
+++ src/functions/getCompanyURL.js
@@ -1,10 +1,10 @@
+import companiesArr from './companiesArr';
+
 export default (company) => {
   if (!company) {
     return null; // or handle the case when company is undefined/null
   }
-
-  const symbols = [['apple', 'AAPL'], ['google', 'GOOG']];
-  const matchingSymbol = symbols.find(([symbol]) => symbol === company);
+  const matchingSymbol = companiesArr.find(([symbol]) => symbol === company);
   const companyName = matchingSymbol ? matchingSymbol[1] : '';
   const url = `https://financialmodelingprep.com/api/v3/income-statement/${companyName}?limit=10&apikey=0b669f139969f23b0a2e01394f344a8f`;
   return url;
