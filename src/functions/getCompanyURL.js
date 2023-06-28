805ca75b1c7a07c0b18df9e464075386a2f94c59
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 17:48:03 +0300

    Update getCompanyURL.js

--- src/functions/getCompanyURL.js
+++ src/functions/getCompanyURL.js
@@ -6,6 +6,6 @@ export default (company) => {
   }
   const matchingSymbol = companiesArr.find(([symbol]) => symbol === company);
   const companyName = matchingSymbol ? matchingSymbol[1] : '';
-  const url = `https://financialmodelingprep.com/api/v3/income-statement/${companyName}?limit=10&apikey=0b669f139969f23b0a2e01394f344a8f`;
+  const url = `https://financialmodelingprep.com/api/v3/income-statement/${companyName}?limit=5&apikey=0b669f139969f23b0a2e01394f344a8f`;
   return url;
 };
