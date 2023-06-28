32daec52b30f118e7264b90b207aeef35403e629
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 11:55:04 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,34 @@
+import { useLocation, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Navbar = () => (
-  <Section className="shadow-md w-full top-0 left-0 bg-navbarBg">
-    <ul className="flex items-center justify-between py-4 px-10 text-white">
-      <li>
-        <ion-icon name="arrow-back-outline" />
-      </li>
-      <li>
-        home
-      </li>
-      <li>
-        <span>
-          <ion-icon name="mic-outline" />
-        </span>
-        <span>
-          <ion-icon name="settings-outline" />
-        </span>
-      </li>
-    </ul>
-  </Section>
-);
+const Navbar = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  const pathNameSplit = pathname.split('/');
+  const newPath = pathNameSplit[pathNameSplit.length - 2];
+  console.log(pathname);
+  console.log(newPath);
+  return (
+    <Section className="shadow-md w-full top-0 left-0 bg-navbarBg">
+      <ul className="flex items-center justify-between py-4 px-10 text-white">
+        <NavLink to={`/${newPath}`}>
+          <ion-icon name="arrow-back-outline" />
+        </NavLink>
+        <li>
+          home
+        </li>
+        <li>
+          <span>
+            <ion-icon name="mic-outline" />
+          </span>
+          <span>
+            <ion-icon name="settings-outline" />
+          </span>
+        </li>
+      </ul>
+    </Section>
+  );
+};
 export default Navbar;
 
 const Section = styled.header`
