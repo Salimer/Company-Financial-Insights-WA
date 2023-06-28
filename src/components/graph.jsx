c99a08668546fa44b4b66d4658fd67c801fb7271
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 16:31:24 +0300

    Update graph.jsx

--- src/components/graph.jsx
+++ src/components/graph.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto';
+import styled from 'styled-components';
 import getRevenueArr from '../functions/getRevenueArr';
 import getYearsArr from '../functions/getYearsArr';
 
@@ -13,14 +14,44 @@ function Graph({ company }) {
     labels: yearsArr,
     datasets: [
       {
-        label: 'Revenue',
+        label: 'Revenue in millions USD',
         data: revenueArr,
+        backgroundColor: ['#4268af', '#3f62a5'],
+        borderColor: 'white',
+        borderWidth: 2,
       },
     ],
   };
 
+  const options = {
+    plugins: {
+      legend: {
+        labels: {
+          color: 'white', // Set the legend text color
+        },
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: 'white', // Set the x-axis ticks color
+        },
+      },
+      y: {
+        ticks: {
+          color: 'white', // Set the y-axis ticks color
+        },
+      },
+    },
+  };
+
   return (
-    <Bar data={data} />
+    <Section className="bg-headlineBg flex flex-col items-center">
+      <h1 className="text-white text-lg p-5 md:p-10">Revenue for the past 5 years</h1>
+      <div className="p-3 w-full">
+        <Bar data={data} options={options} />
+      </div>
+    </Section>
   );
 }
 
@@ -29,3 +60,7 @@ Graph.propTypes = {
 };
 
 export default Graph;
+
+const Section = styled.section`
+
+`;
