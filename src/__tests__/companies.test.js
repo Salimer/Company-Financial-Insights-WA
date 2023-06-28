8420b64659539c2466186eed719a511093e303f8
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-28 21:07:13 +0300

    Create companies.test.js

---
+++ src/__tests__/companies.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Companies from '../components/companies';
+import Button from '../components/Button';
+
+test('renders the Companies component', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <Companies />
+    </MemoryRouter>
+  );
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('renders the Button component', () => {
+  const props = {
+    children: 'Test Button',
+    url: '/test-url',
+    iconName: 'test-icon',
+  };
+  const { container } = render(
+    <MemoryRouter>
+        <Button {...props} />
+    </MemoryRouter>
+  );
+  expect(container.firstChild).toMatchSnapshot();
+});
